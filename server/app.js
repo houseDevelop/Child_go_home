3a1778f461e394bc8b14e36cc4df6c9d027f92b1
Doraimon-Dant <alucart9@gmail.com>
2023-09-20 10:07:19 -0700

    route 404

--- server/app.js
+++ server/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+  res.status(404).sendFile(__dirname+'/public/404.html'
+  )
+})
+
 app.get('/',(req,res)=>{
   res.send('hello world')
 })
+
 app.get('/servicios',(req,res)=>{
   res.send('esta es la pagina de servicios')
 })
-app.use(express.static(__dirname+'/public'))
+
 app.listen(port,()=>{
   console.log(`example app listening at http://localhost:${port}`)
 })
