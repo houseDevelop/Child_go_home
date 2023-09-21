549d65e69816fd4704085dd9b46256facfeaf440
Doraimon-Dant <alucart9@gmail.com>
2023-09-20 17:03:19 -0700

    path fixed

--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const path = require('path')
 const app = express();
+
 const port = 3000;
 
 
 app.use(express.static(__dirname+'/public'));
 
-app.use((req,res,next)=>{
-  res.status(404).sendFile(__dirname+'/public/404.html'
-  )
-})
+
 
 app.get('/',(req,res)=>{
   res.send('hello world')
@@ -16,6 +15,10 @@ app.get('/',(req,res)=>{
 
 app.get('/servicios',(req,res)=>{
   res.send('esta es la pagina de servicios')
+})
+app.use((req,res,next)=>{
+  res.status(404).sendFile(path.join(__dirname,'../public/404.html'
+  ))
 })
 
 app.listen(port,()=>{
